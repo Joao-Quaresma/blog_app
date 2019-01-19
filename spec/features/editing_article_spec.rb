dd87522777ca2ae6c1c28ccd7374d838f58d6f7e
JoaoQuaresma <joao.quaresma1991@gmail.com>
2019-01-19 23:59:35 +0000

    fix broken editing article spec

--- spec/features/editing_article_spec.rb
+++ spec/features/editing_article_spec.rb
@@ -2,7 +2,9 @@ require "rails_helper"
 
 RSpec.feature "Editing an Article" do
   before do
-    @article = Article.create(title: "The first article", body: "something")
+    @john = User.create(email: "john@something.com", password: "password")
+    login_as(@john)
+    @article = Article.create(title: "The first article", body: "something", user: @john)
   end
 
   scenario "A user updates an article" do
